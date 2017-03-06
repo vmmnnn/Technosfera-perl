485a161662d98f32835257781036adfd9bfc1d0c
Марина <vmmnnn@mail.ru>
2017-03-06 19:05:16 +0300

    Add files via upload

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -13,11 +13,13 @@ sub calculate {
 	my @people;     # All members
 	my %hash;       # Answer
 	my %married;    # Married members
+	my $amm_married = 0;
 	my $i = 0;
 	for my $elem (@members) {
 	   if (ref $elem) {    # Married people - put in %married
               $married{$elem->[0]} = $elem->[1];
               $married{$elem->[1]} = $elem->[0];
+	      $amm_married += 2;
 	      $people[$i] = $elem->[0];
 	      $people[$i+1] = $elem->[1]; 
 	      $i += 2;
@@ -27,26 +29,28 @@ sub calculate {
 	   } 
 	}
 
-	for my $person (@people) {   
-	   my @santa_list;    # People, who can give a present to this person
-           for my $santa (@people) {
-	                   # Person can't give presents to himself
-	      next if ($santa eq $person);
-	                   # Santa is already a santa for somebody
-	      next if (exists($hash{$santa}));
-	                   # Santa can't give a present to his santa
-	      next if (exists($hash{$person}) && $hash{$person} eq $santa);
-	                   # Santa can't give a present to his wife/husband
-	      next if (exists($married{$santa}) && 
-	                       $married{$santa} eq $person);
-	      push (@santa_list, $santa);
-	   }
-	   my $num = int(rand(@santa_list));
-	   $hash{$santa_list[$num]} = $person;
+	if (!(@people <= 2 || (@people == 3 && $amm_married == 2))) {
+           for my $person (@people) {   
+   	      my @santa_list;    # People, who can give a present to this person
+              for my $santa (@people) {
+	                      # Person can't give presents to himself
+	         next if ($santa eq $person);
+	                      # Santa is already a santa for somebody
+	         next if (exists($hash{$santa}));
+	                      # Santa can't give a present to his santa
+	         next if (exists($hash{$person}) && $hash{$person} eq $santa);
+	                      # Santa can't give a present to his wife/husband
+	         next if (exists($married{$santa}) && 
+	                          $married{$santa} eq $person);
+	         push (@santa_list, $santa);
+	      }
+	      my $num = int(rand(@santa_list));
+	      $hash{$santa_list[$num]} = $person;
+           }
+           for my $person (@people) {
+              push(@res, [$person, $hash{$person}]);
+   	   }
         }
-        for my $person (@people) {
-           push(@res, [$person, $hash{$person}]);
-	}
 	#	push @res,[ "fromname", "toname" ];
 	# ...
 	return @res;
