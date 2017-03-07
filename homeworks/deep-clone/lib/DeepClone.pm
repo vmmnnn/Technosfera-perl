66a8b922890b89068f3c75d171840e24c4d77b98
Марина <vmmnnn@mail.ru>
2017-03-07 23:06:12 +0300

    Add files via upload

--- homeworks/deep-clone/lib/DeepClone.pm
+++ homeworks/deep-clone/lib/DeepClone.pm
@@ -37,7 +37,92 @@ use warnings;
 sub clone {
 	my $orig = shift;
 	my $cloned;
+	my $ref_type = ref $orig;
+	my $error_fl = 0;
 	# ...
+	if ($ref_type) {
+	    if ($ref_type eq 'ARRAY') {
+	        my @mass;
+		my $i = 0;
+		my @orig_mass = @{$orig};
+		for my $elem (@orig_mass) {
+		    my $ref_elem_type = ref $elem;
+		    my $answ;
+		    if ($ref_elem_type) {
+		        if ($ref_elem_type eq 'ARRAY') {
+		            if ($elem == $orig) {
+		                $mass[$i] = \@mass;
+		            } else {
+		                $answ = clone($elem);
+				if (defined $answ) {
+				    $mass[$i] = $answ;
+				} else {
+				    $error_fl = 1;
+				}
+			    }
+			} elsif ($ref_elem_type eq 'HASH') { 
+		            $answ = clone($elem);
+			    if (defined $answ) {
+			        $mass[$i] = $answ;
+			    } else {
+			        $error_fl = 1;
+			    }
+			 #   $mass[$i] = clone($elem);
+			} else {
+			    $mass[$i] = undef;
+	                    $error_fl = 1;
+			}
+		    } else {
+		       $mass[$i] = $elem;
+		    }
+		    $i++;
+		}
+		$cloned = \@mass;
+	    } elsif ($ref_type eq 'HASH') {
+		my %hash;
+                my %orig_hash = %{$orig};
+		my $answ;
+		for my $key (keys %orig_hash) {
+		    my $val = $orig_hash{$key};
+		    my $ref_val_type = ref $val;
+		    if ($ref_val_type) {
+ 		        if ($ref_val_type eq 'HASH') {
+		            if ($val == $orig) {
+			        $hash{$key} = $val;
+ 			    } else {
+		                $answ = clone($val);
+				if (defined $answ) {
+				    $hash{$key} = $answ;
+				} else {
+				    $error_fl = 1;
+				}
+			    }
+		        } elsif ($ref_val_type eq 'ARRAY') {
+		            $answ = clone($val);
+			    if (defined $answ) {
+			        $hash{$key} = $answ;
+			    } else {
+			        $error_fl = 1;
+			    }
+			#    $hash{$key} = clone($val);
+		        } else {
+			    $hash{$key} = undef;
+	                    $error_fl = 1;
+		        }
+		    } else {
+			$hash{$key} = $val;
+		    }
+		}
+	        $cloned = \%hash;
+	    } else {
+	        $cloned = undef;	    
+	    }  
+	} else {
+	    $cloned = $orig;
+	}
+	if ($error_fl == 1) {
+	    return undef;
+	}
 	# deep clone algorith here
 	# ...
 	return $cloned;
